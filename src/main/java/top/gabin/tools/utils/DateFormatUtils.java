37f8298fcec8265383912418bf51f1be22b70a0b
gabin <18649660797@163.com>
2020-08-16 19:44:07 +0800

    日期格式化改为线程变量模式

--- src/main/java/top/gabin/tools/utils/DateFormatUtils.java
+++ src/main/java/top/gabin/tools/utils/DateFormatUtils.java
@@ -6,13 +6,18 @@ import java.util.Locale;
 
 public class DateFormatUtils {
 
+    // 使用线程变量，避免出现并发问题，SimpleDateFormat非线程安全
+    private static volatile ThreadLocal<SimpleDateFormat> simpleDateFormatThreadLocal =
+            ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.US));
+
     /**
      * 格式化日期为RFC3339格式，某些接口需要用
      * @param date
      * @return
      */
     public static String formatRFC3339ToString(Date date) {
-        return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.US).format(date);
+        SimpleDateFormat simpleDateFormat = simpleDateFormatThreadLocal.get();
+        return simpleDateFormat.format(date);
     }
 
 }
