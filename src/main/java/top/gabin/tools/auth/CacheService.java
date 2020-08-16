d90eba560ed1b30ae52056ecf95a6009f0ef1256
gabin <18649660797@163.com>
2020-08-16 23:16:38 +0800

    没用到返回值就去掉

--- src/main/java/top/gabin/tools/auth/CacheService.java
+++ src/main/java/top/gabin/tools/auth/CacheService.java
@@ -3,7 +3,7 @@ package top.gabin.tools.auth;
 import java.util.List;
 
 public interface CacheService {
-    <T> T cache(String cacheKey, T cacheData);
+    <T> void cache(String cacheKey, T cacheData);
     <T> T get(String cacheKey, Class<T> cacheType);
     <T> List<T> getList(String cacheKey);
 }
