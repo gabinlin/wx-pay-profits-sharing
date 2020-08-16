9f2665d3dad25cfe821d423d70558fde9582eb8f
gabin <18649660797@163.com>
2020-08-16 23:18:32 +0800

    没有用到，也先存着吧

---
+++ src/main/java/top/gabin/tools/request/ecommerce/fund/WithdrawStatusForPlatformRequest1.java
@@ -0,0 +1,33 @@
+package top.gabin.tools.request.ecommerce.fund;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+/**
+ * <pre>
+ * 电商平台通过该接口查询其提现结果，该查询服务提供两种查询方式（两种查询方式返回结果一致）： 方式1：微信支付提现单号查询； 方式2：商户提现单号查询。
+ * 文档地址:https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/ecommerce/fund/chapter3_6.shtml
+ * </pre>
+ */
+@Data
+@JsonIgnoreProperties("outRequestNo")
+public class WithdrawStatusForPlatformRequest1 {
+	/**
+	 * <pre>
+	 * 字段名：商户提现单号
+	 * 变量名：out_request_no
+	 * 是否必填：是
+	 * 类型：string[1,32]
+	 * 描述：
+	 *  path 商户提现单号，由商户自定义生成。 
+	 *  示例值：20190611222222222200000000012122 
+	 * </pre>
+	 */
+	@JsonIgnore
+	@JsonProperty(value = "out_request_no")
+	private String outRequestNo;
+
+}
---
+++ src/main/java/top/gabin/tools/response/pay/combine/CombineTransactionsNotifyResponse.java
@@ -0,0 +1,47 @@
+package top.gabin.tools.response.pay.combine;
+
+import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import top.gabin.tools.response.AbstractResponse;
+
+
+/**
+ * <pre>
+ * 微信支付通过支付通知接口将用户支付成功消息通知给商户
+ * 文档地址:https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/combine/chapter3_7.shtml
+ * </pre>
+ */
+@Data
+@JsonIgnoreProperties()
+public class CombineTransactionsNotifyResponse extends AbstractResponse {
+	/**
+	 * <pre>
+	 * 字段名：返回状态码
+	 * 变量名：code
+	 * 是否必填：是
+	 * 类型：string[1,32]
+	 * 描述：
+	 *  错误码，SUCCESS为清算机构接收成功，其他错误码为失败。 
+	 *  示例值：SUCCESS 
+	 * </pre>
+	 */
+	@JsonProperty(value = "code")
+	private String code;
+
+	/**
+	 * <pre>
+	 * 字段名：返回信息
+	 * 变量名：message
+	 * 是否必填：是
+	 * 类型：string[1,64]
+	 * 描述：
+	 *  返回信息，如非空，为错误原因。 
+	 *  示例值：系统错误 
+	 * </pre>
+	 */
+	@JsonProperty(value = "message")
+	private String message;
+
+}
